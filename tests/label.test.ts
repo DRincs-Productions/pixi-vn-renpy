d9aa132454504bcaa9e2abf7f9948253fe8c709b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-27 16:46:25 +0100

    test: add additional label tests for Ren'py conversion

--- tests/label.test.ts
+++ tests/label.test.ts
@@ -15,6 +15,12 @@ test("Label test 1", async () => {
                 {
                     dialogue: "This is a test.",
                 },
+                {
+                    labelToOpen: {
+                        label: "end",
+                        type: "jump",
+                    },
+                },
             ],
             end: [
                 {
@@ -30,6 +36,43 @@ label start:
 	"This is a test."
 label end:
 	"Goodbye, World!"
+`);
+    expect(res).toEqual(expected);
+});
+
+test("Label test 2", async () => {
+    let expected: PixiVNJson = {
+        labels: {
+            back_in_london: [
+                {
+                    dialogue: "We arrived into London at 9.45pm exactly.",
+                },
+                {
+                    labelToOpen: {
+                        label: "hurry_home",
+                        type: "call",
+                    },
+                },
+            ],
+            hurry_home: [
+                {
+                    dialogue: "We hurried home to Savile Row as fast as we could.",
+                },
+                {
+                    end: "label_end",
+                    goNextStep: true,
+                },
+            ],
+        },
+    };
+    let res = convertRenpyText(`
+label back_in_london:
+    "We arrived into London at 9.45pm exactly."
+    call hurry_home
+
+label hurry_home:
+    "We hurried home to Savile Row as fast as we could."
+    return
 `);
     expect(res).toEqual(expected);
 });
