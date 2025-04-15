5a8317ad57f110de52407819d6287b40b1ec3ac5
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-04-15 12:31:08 +0200

    test: add initial test for Say Statement in convertRenpyText function

---
+++ tests/text_style.test.ts
@@ -0,0 +1,19 @@
+import { PixiVNJson } from "@drincs/pixi-vn-json";
+import { expect, test } from "vitest";
+import { convertRenpyText } from "../src/functions";
+
+test("Say Statement", async () => {
+    let expected: PixiVNJson = {
+        labels: {},
+    };
+    let res = convertRenpyText(`
+define e = Character("Eileen", who_color="#c8ffc8")
+
+label start:
+    "This is narration."
+    "Eileen" "This is dialogue, with an explicit character name."
+    e "This is dialogue, using a character object instead."
+    "Bam!!" with vpunch
+`);
+    expect(res).toEqual(expected);
+});
