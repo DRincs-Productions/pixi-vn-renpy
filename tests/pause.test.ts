206d0b4080950916991f1b8a9090239efca561b0
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-04-15 11:30:48 +0200

    test: add initial test for pause handling in convertRenpyText function

---
+++ tests/pause.test.ts
@@ -0,0 +1,19 @@
+import { PixiVNJson } from "@drincs/pixi-vn-json";
+import { expect, test } from "vitest";
+import { convertRenpyText } from "../src/functions";
+
+test("Pause test 1", async () => {
+    let expected: PixiVNJson = {
+        labels: {},
+    };
+    let res = convertRenpyText(`
+label start:
+    "Hi there! How was class?"
+    "Good..."
+    pause
+    "Are you going home now? Wanna walk back with me?"
+    pause 0.1
+    "Sure!"
+`);
+    expect(res).toEqual(expected);
+});
